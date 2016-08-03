170b95165e83972cf051d7450c788701d13b0f06
kolektiv <andrew@xyncro.com>
2016-08-03 19:12:21 +0100

    renaming

--- conf.py
+++ conf.py
@@ -117,7 +117,7 @@ todo_include_todos = True
 # if not on_rtd:
 #    import sphinx_rtd_theme
 
-html_theme = '_theme'
+html_theme = '_themes'
 html_theme_path = ['.']
 
 #    html_theme = 'sphinx_rtd_theme'
